0b1dead555a762a729680e51ce9c733cedde93b7
DCKAP-L150 <dhanushgdckap@gmail.com>
2023-05-19 11:21:03 +0530

    PrimeNumber problem solved

---
+++ IsPrimeNumber.php
@@ -0,0 +1,30 @@
+<?php
+
+function IsPrimeNumber($input){
+    $isPrime = true;
+if ($input === 1) {
+    echo "1 is neither prime nor composite number.";
+}
+else if ($input > 1) {
+    for ($i = 2; $i < $input; $i++) {
+        if ($input % $i == 0) {
+            $isPrime = false;
+            break;
+        }
+    }
+
+    if ($isPrime) {
+        echo "$input is a prime number";
+    } else {
+        echo "$input is a not prime number";
+    }
+}
+else {
+    echo "The number is not a prime number.";
+}
+    
+}
+
+IsPrimeNumber(13);
+
+?>
