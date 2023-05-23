cee2733b47e02c384b4b573db3105b17a609378c
DCKAP-L150 <dhanushgdckap@gmail.com>
2023-05-23 16:40:09 +0530

    Reverse number task completed.

--- ReverseNumber.php
+++ ReverseNumber.php
@@ -1,6 +1,25 @@
 <?php
 
-$getNumber = (string)readline("Enter the number : ");
-echo strrev($getNumber);
+// This is the method to reverse the number by String method. But we cannot reverse the decimal value.
+
+$ReverseNumberByString = (string)readline("Enter the number : ");
+echo strrev($ReverseNumberByString);
+
+// This is the method by swaping the last value in front to reverse the number. We can also reverse the decimal value.
+
+$getNumber = (int)readline("Enter the number : ");
+function reversDigits($getNumber) {
+    $reverseNumber = 0;
+    while($getNumber > 1) {
+        $reverseNumber = $reverseNumber * 10 + $getNumber % 10;
+        $getNumber = (int)$getNumber / 10;
+    }
+    return $reverseNumber;
+}
+
+echo "Original number is : ".$getNumber;
+echo "\n";
+echo "Reverse of number is ", reversDigits($getNumber);
+echo "\n";
 
 ?>
